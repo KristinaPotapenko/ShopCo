7f57a15589dba152bc4d4c564107b3fb1ba49f92
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-08-22 20:52:19 +0300

    chore: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "SHOP CO",
+  description:
+    "Online store offering stylish clothing for men and women: new arrivals, top sellers, fashion collections, and accessories",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
