472e77486f3dd6206c6fad09ada5fb09fc8c2804
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-08-26 21:40:23 +0300

    feat: integrate Partners component in root page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/sections/Hero/Hero";
+import Partners from "@/components/sections/Partners/Partners";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Partners />
     </main>
   );
 }
