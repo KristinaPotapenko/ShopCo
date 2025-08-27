82f435e10f3c0538233eaae0d8b14b902eee84dc
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-08-27 21:51:45 +0300

    style: add adaptive style for ViewAllButton component

--- src/components/ui/ViewAllButton/ViewAllButton.tsx
+++ src/components/ui/ViewAllButton/ViewAllButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function ViewAllButton() {
   return (
     <Link
-      className="flex items-center justify-center py-3.5 px-20 font-semibold border border-zinc-200 rounded-[60px]"
+      className="flex items-center justify-center md:self-center py-3.5 px-20 font-semibold border border-zinc-200 rounded-[60px]"
       href="/"
     >
       View All
