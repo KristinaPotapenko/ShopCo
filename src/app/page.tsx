28b8c4cbcca5a755ff0cb3c0620792a4735c2814
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-08-27 21:59:42 +0300

    feat: integrate Bestsellers component in root page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/sections/Hero/Hero";
 import Partners from "@/components/sections/Partners/Partners";
 import NewArrivals from "@/components/sections/NewArrivals/NewArrivals";
+import Bestsellers from "@/components/sections/Bestsellers/Bestsellers";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Partners />
       <NewArrivals />
+      <Bestsellers />
     </main>
   );
 }
