dae01c16f2d744bd43cb3e0c392c3061a874895e
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-08-27 21:57:11 +0300

    feat: integrate NewArrivals component in root page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/sections/Hero/Hero";
 import Partners from "@/components/sections/Partners/Partners";
+import NewArrivals from "@/components/sections/NewArrivals/NewArrivals";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Partners />
+      <NewArrivals />
     </main>
   );
 }
