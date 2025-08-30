8d8012379987d48e42564659f2dc515b3bbc34e0
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-08-30 13:31:02 +0300

    style: add hover, active and focus states to ViewAllButton component

--- src/components/ui/ViewAllButton/ViewAllButton.tsx
+++ src/components/ui/ViewAllButton/ViewAllButton.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 export default function ViewAllButton() {
   return (
     <Link
-      className="flex items-center justify-center md:self-center py-3.5 px-20 font-semibold border border-zinc-200 rounded-[60px]"
+      className="flex items-center justify-center md:self-center py-3.5 px-20 font-semibold border border-zinc-200 rounded-[60px]
+             hover:ring-2 hover:ring-offset-2 hover:ring-zinc-200
+              focus:ring-2 focus:ring-offset-2 focus:ring-zinc-200 focus:outline-none
+              active:bg-zinc-100 transition-colors duration-300"
       href="/"
     >
       View All
