109b305def049b6223e2aa2adcc6cab163be0653
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-08-30 13:14:33 +0300

    feat: add hover, active and focus states to Link in HeroInfo component

--- src/components/sections/Hero/HeroInfo/HeroInfo.tsx
+++ src/components/sections/Hero/HeroInfo/HeroInfo.tsx
@@ -12,7 +12,12 @@ export default function HeroInfo() {
         style.
       </p>
       <Link
-        className="flex items-center justify-center py-4 px-16 md:mb-6 text-white font-semibold bg-black rounded-[60px]"
+        className="flex items-center justify-center py-4 px-16 md:mb-6 text-white font-semibold bg-black rounded-[60px]
+        transition-all duration-200 
+             hover:bg-zinc-800 
+             active:scale-95 active:bg-zinc-900 
+             focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black
+        "
         href="/"
       >
         Shop Now
