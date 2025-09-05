fb0374b83b4186ba8dd74b767ffd63e4b04c1172
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-09-05 20:46:34 +0300

    feat: adapt ReviewCard to support product reviews

--- src/components/ui/cards/ReviewCard/ReviewCard.tsx
+++ src/components/ui/cards/ReviewCard/ReviewCard.tsx
@@ -1,16 +1,29 @@
 import Stars from "../../Stars/Stars";
 
-import { Check } from "lucide-react";
+import { Check, Ellipsis as MoreIcon } from "lucide-react";
 
 interface ReviewCardProps {
   rating: number;
   name: string;
   review: string;
+  date?: string;
+  showActions?: boolean;
 }
 
-export default function ReviewCard({ rating, name, review }: ReviewCardProps) {
+export default function ReviewCard({
+  rating,
+  name,
+  review,
+  date,
+  showActions = false,
+}: ReviewCardProps) {
   return (
-    <li className="min-w-full lg:min-w-[auto] lg:flex-[0_0_calc(33.333%_-_16px)] p-6 lg:py-7 lg:px-8 border border-zinc-200 rounded-3xl snap-start">
+    <li className="relative min-w-full lg:min-w-[auto] lg:flex-[0_0_calc(33.333%_-_16px)] p-6 lg:py-7 lg:px-8 border border-zinc-200 rounded-3xl snap-start">
+      {showActions && (
+        <button className="absolute right-4 top-4 text-zinc-400 hover:text-black">
+          <MoreIcon />
+        </button>
+      )}
       <Stars className="mb-3 lg:mb-3.5" />
       <div className="flex items-center gap-1.5 lg:gap-3 lg:mb-3">
         <h3 className="text-base lg:text-xl font-semibold">{name}</h3>
@@ -19,9 +32,10 @@ export default function ReviewCard({ rating, name, review }: ReviewCardProps) {
           strokeWidth={3}
         />
       </div>
-      <p className="font-normal text-zinc-400">
+      <p className={`font-normal text-zinc-400 ${showActions ? "mb-6" : ""}`}>
         <q>{review}</q>
       </p>
+      {date && <p className="font-normal text-black/60">Posted on {date}</p>}
     </li>
   );
 }
