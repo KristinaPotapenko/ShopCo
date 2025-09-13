e19f6e1f69c573ea03b1748623f57adaef7d8b84
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-09-13 20:35:57 +0300

    feat: set hero image priority and add loading animation

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 
 import HeroStatistics from "./HeroStatistics/HeroStatistics";
 import HeroInfo from "./HeroInfo/HeroInfo";
 
 export default function Hero() {
+  const [loadedImage, setLoadedImage] = useState(false);
+
   return (
     <div className="pt-10 bg-[#F2F0F1]">
       <section className="container flex flex-col md:flex-row md:items-center md:justify-between md:gap-8 px-4 mx-auto my-0">
@@ -11,12 +16,24 @@ export default function Hero() {
           <HeroInfo />
           <HeroStatistics />
         </div>
-        <div className="relative w-full h-[500px] md:h-[600px]">
+        <div className="relative w-full h-[500px] md:h-[600px] overflow-hidden">
+          {!loadedImage && (
+            <>
+              <div className="absolute inset-0 rounded-xl bg-gray-50">
+                <div className="absolute inset-9 rounded-lg bg-[#F2F0F1]" />
+                <div className="absolute inset-0 -translate-x-full animate-super-glow-shimmer bg-gradient-to-r from-transparent via-white/100 via-30% to-transparent super-glow-effect" />
+              </div>
+            </>
+          )}
           <Image
             src="/hero-models.jpg"
             alt="Fashionable man and woman showcasing modern clothing"
             fill
-            className="object-cover object-top"
+            priority
+            className={`object-cover object-top transition-opacity duration-500 ${
+              loadedImage ? "opacity-100" : "opacity-0"
+            }`}
+            onLoadingComplete={() => setLoadedImage(true)}
           />
         </div>
       </section>
