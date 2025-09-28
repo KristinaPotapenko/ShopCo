c2c2976e5807f9dbaddd94793732ab33343b21f2
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-09-28 17:24:02 +0300

    feat: add smallSize prop to Loader for compact version

--- src/components/ui/Loader/Loader.tsx
+++ src/components/ui/Loader/Loader.tsx
@@ -1,7 +1,19 @@
-export const Loader = () => {
+interface LoaderProps {
+  smallSize?: boolean;
+}
+
+export const Loader = ({ smallSize }: LoaderProps) => {
   return (
-    <div className="flex items-center justify-center h-[60vh]">
-      <div className="w-40 h-40 border-4 border-t-transparent border-zinc-300 rounded-full animate-spin"></div>
+    <div
+      className={`flex items-center justify-center ${
+        smallSize ? "h-full" : "h-[60vh]"
+      }`}
+    >
+      <div
+        className={`${
+          smallSize ? "w-20 h-20" : "w-40 h-40"
+        } border-4 border-t-transparent border-zinc-300 rounded-full animate-spin`}
+      ></div>
     </div>
   );
 };
